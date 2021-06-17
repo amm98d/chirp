81300855d5a170d25af80704c0cd3abfaf4f695b
SanaFatim <42048353+SanaFatim@users.noreply.github.com>
2021-06-17 16:17:10 +0500

    commit

--- backend/NodeServer/chirp/Server/Server.js
+++ backend/NodeServer/chirp/Server/Server.js
@@ -6,10 +6,10 @@ expapp.use( express.json() );
 expapp.use(express.urlencoded({ extended: true})); 
 const path = require('path');
 const port = process.env.PORT || 8000;
-
+const {MongoClient} = require('mongodb');
 
 expapp.get("/",(req,res) =>{
-   
+   npm
     res.sendFile(path.join(__dirname + '/WelcomePage/welcome.html'));
 });
 
