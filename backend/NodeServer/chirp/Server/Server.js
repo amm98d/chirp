891cb9abfff9dd0596a093c4deb03284122f9842
adil.hassan20 <adil.hassan20@gmail.com>
2021-06-19 10:14:19 +0500

    backend

--- backend/NodeServer/chirp/Server/Server.js
+++ backend/NodeServer/chirp/Server/Server.js
@@ -1,5 +1,3 @@
-const PayableTrans = require('../expressjsblock/PayableTrans');
-const GetterFunctions = require('../expressjsblock/GetterFuncs');
 const express = require('express');
 const expapp = express();
 expapp.use( express.json() );       
@@ -15,127 +13,9 @@ expapp.get("/",(req,res) =>{
 
 
 
-//Server Making Transactions
-expapp.post("/AddUser", async(req,res) =>{
-     console.log(" Request Recieved for adding User: ");
-     console.log(req.body);
-     var trans = await PayableTrans.AddUser(req.body.cnic,req.body.name);
-     console.log(trans);
-     res.send(trans);
- 
- });
 
 
- expapp.post("/AddUserAsset", async(req,res) =>{
-     console.log("Requestrd Recieved for adding User Asset: ");
-     console.log(req.body);
 
-     var trans = await PayableTrans.AddUserAsset(req.body.cnic,req.body.cval,req.body.taxapp,req.body.ptype);
-     console.log(trans);
-     res.send(trans);
-
- 
- });
- expapp.post("/AddUserAssetsValue", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var trans = await PayableTrans.AddUserAssetsValue(req.body.cnic,req.body.val);
-     console.log(trans);
-     res.send(trans);
- 
- });
- expapp.post("/AddUserIncome", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var trans = await PayableTrans.AddUserIncome(req.body.cnic,req.body.inc);
-     console.log(trans);
-     res.send(trans);
- 
- });
- expapp.post("/AddUserPayment", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var trans = await PayableTrans.AddUserPayment(req.body.cnic,req.body.pay);
-     console.log(trans);
-     res.send(trans);
- 
- });
- expapp.post("/RemoveUser", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var trans = await PayableTrans.RemoveUser(req.body.cnic);
-     console.log(trans);
-     res.send(trans);
- 
- });
- expapp.post("/UsersetCurrentTaxDue", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var trans = await PayableTrans.UsersetCurrentTaxDue(req.body.cnic,req.body.tax);
-     console.log(trans);
-     res.send(trans);
- 
- });
- 
-// Getter responses
- expapp.post("/getNumberofUsers", async(req,res) =>{
-    
-     console.log(req.body);
-     var bal = await GetterFunctions.getNumberofUsers();
-     res.send(bal);
- 
- });
- expapp.post("/GetRegisteredCNICs", async(req,res) =>{
-   
-    console.log("Getcnics");
-    
-     var comp = await GetterFunctions.GetRegisteredCNICs();
-     res.send(comp);
- 
- });
- expapp.post("/GetUserAssetsValue", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var bal = await GetterFunctions.GetUserAssetsValue(req.body.cnic);
-     res.send(bal);
- 
- });
- expapp.post("/getUserIncome_M_Y", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var bal = await GetterFunctions.getCompanyName(req.body.cnic);
-     res.send(bal);
- 
- });
- expapp.post("/getUserPayments", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var bal = await GetterFunctions.getUserName(req.body.cnic,req.body.month,req.body.year);
-     res.send(bal);
- 
- });
- expapp.post("/getUserTotalAssets", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var bal = await GetterFunctions.getUserTotalAssets(req.body.cnic);
-     res.send(bal);
- 
- });
- expapp.post("/UsergetCurrentTaxDue", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var bal = await GetterFunctions.getCompamnyUsers(req.body.cnic);
-     res.send(bal);
- 
- });
- expapp.post("/getUser", async(req,res) =>{
-    // console.log(req.body);
-     console.log(req.body);
-     var bal = await GetterFunctions.getTrustedListOfUser(req.body.cnic);
-     res.send(bal);
- 
- });
- 
 expapp.listen(port, () =>{
     console.log(`Server is up on ${port}`);
 });
