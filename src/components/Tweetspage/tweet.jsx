1d28c4a668fdba05e3ae7f6037c12bb4fd4ee491
Ahmed Mustafa Malik <ahmedmustafamalik98@gmail.com>
2021-06-22 13:58:15 +0500

    [Working] Tweak Post Tweet Component

--- src/components/Tweetspage/tweet.jsx
+++ src/components/Tweetspage/tweet.jsx
@@ -1,65 +1,27 @@
 import "./tweet.css"
 
-import {PermMedia, EmojiEmotions} from "@material-ui/icons"
+import { PermMedia, EmojiEmotions } from "@material-ui/icons"
 
 export default function Share() {
     return (
         <>
-      
-                <div>
+            <div>
                 <div className="card" >
-                
-                <div className="card-header">
-                <img className="tweetProfileImg" src="/assets/helmet.jpg" alt="" />
-                
-                </div>
-                    
-                <div className="card-body" >
-                <input placeholder="Tweety Tweets Tweet"className="tweetInput"/>
-                <hr className="tweetHr"/>
-
-                       <PermMedia htmlColor="tomato" className="tweetIcon"/>
-                        <span className="tweetOptionText">Photo or Video</span>
-                        <EmojiEmotions htmlColor="goldenrod" className="tweetIcon"/>
-                        <span className="tweetOptionText">Emoji</span>
-                        <button  className="tweetButton" type="button" >Tweet</button>
+                    <div className="card-header">
+                        <img className="tweetProfileImg" src="/images/smile.svg" alt="" />
+                        <h4 className="chirp-around">Chirp Around</h4>
+                    </div>
+                    <div className="card-body" >
+                        <input placeholder="Chirp chirp chirp" className="tweetInput" />
+                        <hr/>
+                        {/* <PermMedia htmlColor="tomato" className="tweetIcon" /> */}
+                        {/* <span className="tweetOptionText">Photo or Video</span> */}
+                        {/* <EmojiEmotions htmlColor="goldenrod" className="tweetIcon" /> */}
+                        {/* <span className="tweetOptionText">Emoji</span> */}
+                        <button className="tweetButton btn btn-outline-danger" type="button" >Tweet</button>
+                    </div>
                 </div>
             </div>
-
-         </div>
-                
-
-           
- {/* <div className="tweet">
-            
-       <div className="tweetWrapper">
-        <div className="tweetTop">
-          <img className="tweetProfileImg" src="/assets/helmet.jpg" alt="" />
-          <input
-            placeholder="Tweety Tweets Tweet"
-            className="tweetInput"
-          />
-        </div>
-        <hr className="tweetHr"/>
-        <div className="tweetBottom">
-            <div className="tweetOptions">
-                <div className="tweetOption">
-                    <PermMedia htmlColor="tomato" className="tweetIcon"/>
-                    <span className="tweetOptionText">Photo or Video</span>
-                </div>
-                
-                <div className="tweetOption">
-                    <EmojiEmotions htmlColor="goldenrod" className="tweetIcon"/>
-                    <span className="tweetOptionText">Emoji</span>
-                </div>
-            </div>
-            <button className="tweetButton">Tweet</button>
-        </div>
-      </div> 
-</div> */}
-
-
-    
-    </>
+        </>
     );
 }
